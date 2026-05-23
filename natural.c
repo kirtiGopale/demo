0ca2751243f1de027de19b8738c2f6367bd8252f
kirtiGopale <kirti.gopale2506@gmail.com>
2026-05-23 16:28:17 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("\n enter the value");
+    scanf("%d",&n);
+    printf("\n natural number form 1 to %d",n);
+    for(int i=1;i<=n;i++)
+    {
+            printf("\n %d",i);
+
+    }
+    return 0;
+}
+
